9786840f39c53e76752cb83add2a8a2f3224c4ed
Mikhail Polyarush <mvp_devops@mail.ru>
2022-08-19 10:26:53 +0300

    fix

--- server/src/modules/file-storage/index.ts
+++ server/src/modules/file-storage/index.ts
@@ -1,20 +1,12 @@
 import { FileStorageModule } from "./file-storage.module";
 import { FileStorageService } from "./file-storage.service";
 
-import {
-  LogoEntity,
-  SectionEntity,
-  StageEntity,
-  NormativeEntity,
-  DesignDocumentEntity,
-} from "./entities";
+import { LogoEntity, NormativeEntity, DesignDocumentEntity } from "./entities";
 
 export {
   FileStorageService,
   FileStorageModule,
   LogoEntity,
-  SectionEntity,
-  StageEntity,
   NormativeEntity,
   DesignDocumentEntity,
 };
